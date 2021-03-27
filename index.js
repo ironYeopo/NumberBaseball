4b73cf407dab386923d8b444691ead2a6c79fd41
ironYeopo <76603607+ironYeopo@users.noreply.github.com>
2021-03-27 10:28:00 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // 게임시작 버튼을 눌렀을 때 랜덤한 수 생성
 let comArray = [];
+
 function randomN() {
   while(comArray.length !== 3) {
     comArray.push(Math.floor(Math.random() * 10));
@@ -7,6 +8,13 @@ function randomN() {
   return comArray;
 }
 //console.log(randomN());
+
+function ran(array) {
+  if(array.length === 3) return array;
+  array.push(Math.floor(Math.random() * 10));
+  return ran(array);
+}
+//console.log(ran(comArray));
 
 function enter(){
   let input = document.getElementById('num');
@@ -17,27 +25,27 @@ function enter(){
     });
 }
 
-//let inputVal = document.getElementById('num').addEventListener('keydown', compare);
+let inputVal = document.getElementById('num').addEventListener('keydown', compare);
 
-function compare(input,comArray) {    
-  //if(inputVal.keycode === 13)
-  //if(input.length !== 3) alert('input 3 numbers');  
-  //else {  
-    //numToStr(input)
-    let strike = 0;
-    let ball = 0;
-    for(let i = 0; i < comArray.length; i++) {
-      for(let j = 0; j < input.length; j++) {
-        if(i == j && comArray[i] == input[j]) ++strike;
-        else if(i != j && comArray[i] == input[j]) ++ball;
-      }       
+function compare() {    
+  if(inputVal.keycode === 13) {
+    if(input.length !== 3) alert('input 3 numbers');  
+    else {
+      let strike = 0;
+      let ball = 0;r
+      for(let i = 0; i < comArray.length; i++) {
+        for(let j = 0; j < input.length; j++) {
+          if(i == j && comArray[i] == input[j]) ++strike;
+          else if(i != j && comArray[i] == input[j]) ++ball;
+        }       
+      }
     }
-  //}
-  return [strike, ball];
+    return [strike, ball];
+  }
 }
 //console.log(compare(input, array));
 function e(event) {
   let a = event.keycode;
   return a;
 }
-console.log(e('a'));
+//console.log(e('a'));
