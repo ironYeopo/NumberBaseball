28ac7f62c8130aac5710f154c94f3c95c2bde545
ironYeopo <76603607+ironYeopo@users.noreply.github.com>
2021-03-30 01:46:37 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,38 @@
-let comArray;
+// 게임시작 버튼을 눌렀을 때 랜덤한 수 생성
+let comArray = [];
+
 function randomN() {
-    var array = [];
-    while(array.length !== 3) {
-        array.push(Math.floor(Math.random() * 10));
-    }
-    comArray = array;
-    return console.log(comArray);
+  while(comArray.length !== 3) {
+    comArray.push(Math.floor(Math.random() * 10));
+  }
+  return comArray;
 }
 //console.log(randomN());
 
-/*if (input.length !== 3)
-{
-    alert('Input 3 numbers!');
-}*/
-function enter(){
-    let input = document.getElementById('num').value;
-    document.getElementById('num').addEventListener('keydown', function(event) {        
-      if (input.keyCode === 13) {
-        compare();
-      }
-    });
+// 게임시작 버튼을 눌렀을 때 랜덤한 수 생성 v2
+function ran(array) {
+  if(array.length === 3) return array;
+  array.push(Math.floor(Math.random() * 10));
+  return ran(array);
 }
+//console.log(ran(comArray));
 
-function compare() {    
-  /*let input = document.getElementById('num').value;
-    input.addEventListener('keydown', function(event) {        
-      if (input.keyCode === 13) {
-        if(input.length !== 3) {
-          alert('Input 3 numbers!');
-        }else {    
-          var strike = 0;
-          var ball = 0;
-          for(var i = 0; i < comArray.length; i++) {
-            for(var j = 0; j < input.length; j++) {
-              if(i === j && comArray[i] === input[j]) ++strike;
-              else if (i !== j && comArray[i] === input[j]) ++ball;
-            }       
-          }
-        }
-          return console.log([strike, ball]);
-      }
-    });
-}*/
-    
-    if(input.length !== 3) {
-    alert('Input 3 numbers!');
-    }
-    
-    var strike = 0;
-    var ball = 0;
-    for(var i = 0; i < lastArray.length; i++) {
-        for(var j = 0; j < input.length; j++) {
-            if(i === j && lastArray[i] === input[j]) {
-                ++strike;
-            } else if (i !== j && lastArray[i] === input[j]) {
-                ++ball;
-            }
+let inputVal = document.getElementById('num').addEventListener('keydown', compare);
+
+function compare(inputVal) {    
+  if(inputVal.keycode === 13) {
+    if(input.length !== 3) alert('input 3 numbers');  
+    else {
+      let strike = 0;
+      let ball = 0;r
+      for(let i = 0; i < comArray.length; i++) {
+        for(let j = 0; j < input.length; j++) {
+          if(i == j && comArray[i] == input[j]) ++strike;
+          else if(i != j && comArray[i] == input[j]) ++ball;
         }       
+      }
     }
-    return console.log([strike, ball]);
+  return alert([strike, ball]);
+  }
 }
-//console.log(compare(array2, input));
+//console.log(compare(input, array));
